37524ec9c837982b8397cc813425bb4fab3d3a18
Lautaro Beltramone <lautarobeltra@gmail.com>
2023-03-14 18:41:22 -0300

    ejercicio java

---
+++ javatest/javaintro01.java
@@ -0,0 +1,19 @@
+package javatest;
+
+public class javaintro01 {
+public static void main(String[] args) {
+    
+    int num1 = 5;
+	int num2 = 5;
+    
+	int suma = num1 + num2;
+    System.out.println(suma);
+	double division = num1 / num2;
+    System.out.println(division);
+	boolean logico = num2 < num1;
+    System.out.println(logico);
+    
+	num1++;
+    System.out.println(num1);
+    
+}}
