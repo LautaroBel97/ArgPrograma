66c23813b40d558a0a890b52f4d631084457db77
Lautaro Beltramone <lautarobeltra@gmail.com>
2023-03-15 18:12:22 -0300

    ejercicio 2

--- guia 1 ejercicios/ejerciciosguia1/ejerciciosguia1.java
+++ guia 1 ejercicios/ejerciciosguia1/ejerciciosguia1.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class ejerciciosguia1 {
 
     public static void main(String[] agrs) {
-        try (Scanner leer = new Scanner(System.in)) {
+
+        //EJERCICIO 1
+        /* try (Scanner leer = new Scanner(System.in)) {
             String num1;
             String num2;
             System.out.println("ingrese dos numeros enteros");
@@ -16,8 +18,15 @@ public class ejerciciosguia1 {
             int numEntero2 = Integer.parseInt(num2);
             int suma = numEntero1 + numEntero2;
             System.out.println("La suma es " + suma);
-        }
+        } */
         
+        //EJERCICIO 2
+        try (Scanner leer = new Scanner(System.in)) {
+            String nombre;
+            System.out.println("Ingresa tu nombre");
+            nombre = leer.next();
+            System.out.println("Tu nombre es " + nombre );
+        }
 
     }
 }
