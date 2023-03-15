559f4a17c0385fba32d66257a38da1708eb5ce5b
Lautaro Beltramone <lautarobeltra@gmail.com>
2023-03-15 15:42:44 -0300

    mas ejercicios java

--- javatest/javaintro01.java
+++ javatest/javaintro01.java
@@ -4,16 +4,22 @@ public class javaintro01 {
 public static void main(String[] args) {
     
     int num1 = 5;
-	int num2 = 5;
+	int num2 = 10;
     
 	int suma = num1 + num2;
     System.out.println(suma);
-	double division = num1 / num2;
+	double division = num2 / num1;
     System.out.println(division);
-	boolean logico = num2 < num1;
+	boolean logico = num2 > num1;
     System.out.println(logico);
     
 	num1++;
     System.out.println(num1);
+    System.out.println("hello world");
+
+    String nombre = "Lautaro Beltramone";
+    int edad = 25;
+    System.out.println("Mi nombre es " + nombre + " y mi edad es " + edad + " años");
     //comentario de prueba
-}}
+}
+}
